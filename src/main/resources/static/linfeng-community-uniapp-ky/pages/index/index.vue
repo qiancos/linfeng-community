3669d616f75ce7acccb2e36d154a62bca94e71a6
linfeng <2445465217@qq.com>
2022-07-26 17:52:37 +0800

    uniapp用户端初始化

---
+++ src/main/resources/static/linfeng-community-uniapp-ky/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		
+		<u-icon name="photo"></u-icon>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
